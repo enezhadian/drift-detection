21f090d3474d51ddab8e959586e0c03caee31fa6
Ehsan Nezhadian <enezhadian@gmail.com>
2017-08-24 19:28:01 +0200

    [WIP] Add CDCStream.DriftDetector.

---
+++ src/main/java/CDCStream/DriftDetector.java
@@ -0,0 +1,40 @@
+/*
+ *      CDCStream/DriftDetector.java
+ *      Drift Detection
+ *
+ *  Copyright 2017 Ehsan Nezhadian
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package CDCStream;
+
+import java.util.NoSuchElementException;
+
+import DataStreamReader.TabularStreamReader;
+
+
+public class DriftDetector {
+
+    public DriftDetector(TabularStreamReader stream, int blockSize) {}
+
+    public void run() {
+        try {
+            while (true) {}
+        } catch (NoSuchElementException e) {
+            System.out.println("Done.");
+        }
+    }
+
+}
