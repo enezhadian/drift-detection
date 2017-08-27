93da08652fa719a2646609088f2b3974d80ae695
Ehsan Nezhadian <enezhadian@gmail.com>
2017-08-27 16:44:48 +0200

    Calculate distance matrix.

--- src/main/java/CDCStream/DILCA.java
+++ src/main/java/CDCStream/DILCA.java
@@ -33,79 +33,71 @@ class DILCA {
      *                        STATIC MEMBERS AND METHODS                        *
      *--------------------------------------------------------------------------*/
 
+    // TODO: Make sure this is fast enough.
     public static DILCA distanceMatrixFor(ImmutableList<ImmutableList<String>> database,
                                           int targetAttributeIndex) {
-        // TODO: Implement this.
-        return null;
-//        // Find value domain for target attribute.
-//        ImmutableSet.Builder<String> domainBuilder = ImmutableSet.builder();
-//        for (ImmutableList<String> record : database) {
-//            domainBuilder.add(record.get(targetAttributeIndex));
-//        }
-//        ImmutableSet<String> domain = domainBuilder.build();
-//
-//        // Build distance matrix.
-//        DILCAMatrix distanceMatrix = new DILCAMatrix(domain);
-//
-//
-//        // Find context attributes.
-//        int[] contextAttributeIndexes = contextAttributeIndexesFor(database, targetAttributeIndex);
-//
-//        Map<String, Map<String, Integer>> cooccurrencesMap = new HashMap<>();
-//        String contextValue, targetValue;
-//        Map<String, Integer> targetMap;
-//        double totalContextDomainSizes = 0;
-//        double currentSum, difference;
-//
-//        for (int contextAttributeIndex : contextAttributeIndexes) {
-//            // Clear co-occurrences map to use it for current context attribute.
-//            cooccurrencesMap.clear();
-//
-//            // Count co-occurrences of context and target values.
-//            for (ImmutableList<String> record : database) {
-//                contextValue = record.get(contextAttributeIndex);
-//                targetValue = record.get(targetAttributeIndex);
-//
-//                targetMap = cooccurrencesMap.getOrDefault(contextValue, new HashMap<>());
-//                targetMap.put(targetValue, targetMap.getOrDefault(targetValue, 0) + 1);
-//                cooccurrencesMap.put(contextValue, targetMap);
-//            }
-//
-//            totalContextDomainSizes += cooccurrencesMap.size();
-//
-//            // Calculate the sum of squared differences over all the values of current context attribute.
-//            for (String firstValue : domain) {
-//                for (String secondValue : domain) {
-//                    if (firstValue.compareTo(secondValue) < 0) {
-//                        for (String value : cooccurrencesMap.keySet()) {
-//                            targetMap = cooccurrencesMap.get(value);
-//
-//                            currentSum = distanceMatrix.get(firstValue, secondValue);
-//                            difference = (targetMap.getOrDefault(firstValue, 0) -
-//                                    targetMap.getOrDefault(secondValue, 0));
-//                            distanceMatrix.set(firstValue, secondValue, currentSum + (difference * difference));
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        // Normalize sum of squared differences.
-//        for (String firstValue : domain) {
-//            for (String secondValue : domain) {
-//                if (firstValue.compareTo(secondValue) < 0) {
-//                    currentSum = distanceMatrix.get(firstValue, secondValue);
-//                    distanceMatrix.set(firstValue, secondValue, Math.sqrt(currentSum / totalContextDomainSizes));
-//                }
-//            }
-//        }
-//
-//        return distanceMatrix;
+        if (database.size() == 0) {
+            throw new IllegalArgumentException("Empty database.");
+        }
+
+        int numAttributes = database.get(0).size();
+        // Calculate the co-occurrences of target values with attribute values.
+        List<Map<String, Map<String, Integer>>> cooccurrences = cooccurrencesIn(
+                database, numAttributes, targetAttributeIndex);
+
+
+        // Build the distance matrix.
+        Map<String, Map<String, Double>> distances = new HashMap<>();
+
+        // Find context attributes.
+        int[] contextAttributeIndexes = contextAttributeIndexesFor(cooccurrences);
+
+        Map<String, Map<String, Integer>> attributeCooccurrences;
+        Map<String, Integer> valueCooccurrences;
+        Map<String, Double> valueDistances;
+
+        double totalContextDomainSizes = 0;
+        for (int attributeIndex : contextAttributeIndexes) {
+            attributeCooccurrences = cooccurrences.get(attributeIndex);
+
+            // Calculate total sum of domain sizes for all attributes.
+            totalContextDomainSizes += attributeCooccurrences.size();
+
+            // Calculate the sum of squared differences over all the values of current context attribute.
+            for (String attributeValue : attributeCooccurrences.keySet()) {
+                valueCooccurrences = attributeCooccurrences.get(attributeValue);
+
+                for (String firstValue : valueCooccurrences.keySet()) {
+                    for (String secondValue : valueCooccurrences.keySet()) {
+                        if (firstValue.compareTo(secondValue) < 0) {
+                            valueDistances = distances.getOrDefault(firstValue, new HashMap<>());
+
+                            double currentSum = valueDistances.getOrDefault(secondValue, 0.0);
+                            double difference = valueCooccurrences.getOrDefault(firstValue, 0) -
+                                    valueCooccurrences.getOrDefault(secondValue, 0);
+                            valueDistances.put(secondValue, currentSum + (difference * difference));
+                        }
+                    }
+                }
+            }
+        }
+
+        // Normalize sum of squared differences.
+        for (String firstValue : distances.keySet()) {
+            valueDistances = distances.get(firstValue);
+
+            for (String secondValue : valueDistances.keySet()) {
+                double currentSum = valueDistances.get(secondValue);
+                valueDistances.put(secondValue, Math.sqrt(currentSum / totalContextDomainSizes));
+            }
+        }
+
+        return new DILCA(distances);
     }
 
-    public static List<Map<String, Map<String, Integer>>> cooccurrences(ImmutableList<ImmutableList<String>> database,
-                                                                        int numAttributes,
-                                                                        int targetAttributeIndex) {
+    private static List<Map<String, Map<String, Integer>>> cooccurrencesIn(ImmutableList<ImmutableList<String>> database,
+                                                                           int numAttributes,
+                                                                           int targetAttributeIndex) {
         // Data structure holding the co-occurrences of target values and attribute values for all the attributes.
         List<Map<String, Map<String, Integer>>> cooccurrences = new ArrayList<>(numAttributes);
         for (int i = 0; i < numAttributes; i++) {
@@ -137,89 +129,38 @@ class DILCA {
         return cooccurrences;
     }
 
+    private static int[] contextAttributeIndexesFor(List<Map<String, Map<String, Integer>>> cooccurrences) {
+        // TODO: Implement this.
+        return null;
+    }
+
 
     /*--------------------------------------------------------------------------*
      *                       INSTANCE MEMBERS AND METHODS                       *
      *--------------------------------------------------------------------------*/
 
     public double get(String firstValue, String secondValue) {
-        // Get indexes corresponding to given values.
-        int firstIndex = valueMap.getOrDefault(firstValue, -1);
-        int secondIndex = valueMap.getOrDefault(secondValue, -1);
-
-        if (firstIndex < 0 || secondIndex < 0) {
-            throw new IndexOutOfBoundsException("At least one of the values are out of the domain.");
-        }
-
-        // Make sure `firstIndex` is the smaller index.
-        if (firstIndex == secondIndex) {
-            // Diagonal is always zero.
+        int comparison = firstValue.compareTo(secondValue);
+        if (0 == comparison) {
+            // Make sure `firstValue` is a valid value.
+            distances.get(firstValue);
             return 0;
-        } else if (firstIndex > secondIndex) {
-            int temp = firstIndex;
-            firstIndex = secondIndex;
-            secondIndex = temp;
         }
 
-        return distances[firstIndex][secondIndex - firstIndex];
+        // Make sure `firstValue` is smaller than `secondValue`.
+        if (0 > comparison) {
+            String temp = firstValue;
+            firstValue = secondValue;
+            secondValue = temp;
+        }
+
+        return distances.get(firstValue).get(secondValue);
     }
 
-    private final ImmutableMap<String, Integer> valueMap;
-    private final double[][] distances;
+    Map<String, Map<String, Double>> distances;
 
-    private DILCA(Set<String> domain) {
-        if (domain.size() == 0) {
-            throw new IllegalArgumentException("Domain of values cannot be empty.");
-        }
-
-        // Store attribute value domain.
-        ImmutableMap.Builder<String, Integer> mapBuilder = ImmutableMap.builder();
-        int index = 0;
-        for (String value : domain) {
-            mapBuilder.put(value, index);
-            index++;
-        }
-        this.valueMap = mapBuilder.build();
-
-        // Create a two dimensional structure to store upper triangle of distance matrix.
-        int size = this.valueMap.size() - 1;
-
-        this.distances = new double[size][];
-        for (int i = 0; i < size; i++) {
-            this.distances[i] = new double[size - i];
-        }
-    }
-
-    private void set(String firstValue, String secondValue, double distance) {
-        // Get indexes corresponding to given values.
-        int firstIndex = valueMap.getOrDefault(firstValue, -1);
-        int secondIndex = valueMap.getOrDefault(secondValue, -1);
-
-        if (firstIndex < 0 || secondIndex < 0) {
-            throw new IndexOutOfBoundsException("At least one of the values are out of the domain.");
-        } else if (firstIndex == secondIndex) {
-            throw new IllegalArgumentException("Diagonal can't be set.");
-        }
-
-        // Make sure `firstIndex` is the smaller index.
-        if (firstIndex > secondIndex) {
-            int temp = firstIndex;
-            firstIndex = secondIndex;
-            secondIndex = temp;
-        }
-
-        distances[firstIndex][secondIndex - firstIndex] = distance;
-    }
-
-    private double normalizedSquaredSum() {
-        double sum = 0, numValues = 0;
-        for (double[] row : distances) {
-            for (double value : row) {
-                sum += value * value;
-                numValues++;
-            }
-        }
-        return sum / numValues;
+    private DILCA(Map<String, Map<String, Double>> distances) {
+        this.distances = distances;
     }
 
 }
